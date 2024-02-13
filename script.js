40354a9a42fa8a83672133d9a2a0e461b97e440d
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-13 22:13:38 +0100

    vymazani obsahu

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/*let hodinovka = 320
-let pracovniDoba = 7
-let pracovniDny = 21
-let mesicniPrijem = hodinovka * pracovniDoba * pracovniDny
-
-console.log (hodinovka * pracovniDoba * pracovniDny);
-
-document.body.innerHTML = mesicniPrijem
-
-let zivnostPausal = mesicniPrijem * 0.4
-let dan = zivnostPausal * 0.15
-
-console.log (Math.floor (dan))
-; 
-*/
-
-
-
 /*V programu kin se často uvádí délka filmu v minutách.
 Například rozšířená verze filmu :i[Pán prstenů: Dvě věže] trvá 223 minut.
 My bychom ovšem délku filmu raději věděli v hodinách a minutách.
