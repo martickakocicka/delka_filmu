c058cbaa41ce7ca78148cf765a3164471fe6485a
Marta Bakešová <marta.bakesova@centrum.cz>
2024-02-13 22:01:53 +0100

    vymazani obsahu

--- script.js
+++ script.js
@@ -39,22 +39,3 @@ console.log ("zbytek minut " + zbyvajiciMinuty);
 
 document.body.innerHTML= "Dvě Věže mají " + delkaVHodinach + " hodiny a " + zbyvajiciMinuty + " minut."
 
-
-/*Sestavte e-mailovou adresu tak, že sečtete dohromady vaše křestní jméno,
-znak tečka, vaše příjmení a koncovku @mujmail.com.
-Celou e-mailovou adresu vypište do stránky.*/
-
-
-/*let jmeno= "marta"
-let prijmeni= "bakesova"
-let koncovka= "mujmail.com"
-
-document.body.innerHTML= jmeno + "." + prijmeni +"@"+ koncovka
-*/
-
-let mail= "marta" + "." + "bakesova" + "@" + "mujmail.com"
-document.body.innerHTML += mail;
-
-//+= znamena pricti k puvodni i to nove (u cisle by se ale secetly)
-
-// 31/1 2.hodina
